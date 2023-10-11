f5e4e19180053778fd56951e07da97680cb52cd1
levanhuy99 <huylv@techasians.com>
2023-10-11 16:11:01 +0700

    install-library

--- src/app/SSO/ant/ant.module.ts
+++ src/app/SSO/ant/ant.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AntRoutingModule } from './ant-routing.module';
 import { AntDesignComponent } from './ant-design/ant-design.component';
-import { NzInputModule } from 'ng-zorro-antd/input';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { NzFormModule } from 'ng-zorro-antd/form';
 
 
 @NgModule({
@@ -14,7 +11,6 @@ import { NzFormModule } from 'ng-zorro-antd/form';
   ],
   imports: [
     CommonModule,
-    AntRoutingModule,
     ReactiveFormsModule,
     FormsModule,
     // NzFormModule,
